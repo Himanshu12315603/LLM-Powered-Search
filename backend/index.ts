7c4e55a9658a5a03139bed1ae419e732f718089e
Himanshu12315603 <himanshukumar7349820018@gmail.com>
2026-04-27 12:36:49 +0530

    add: backend file

---
+++ backend/index.ts
@@ -0,0 +1,17 @@
+import express from 'express';
+
+
+const app = express();
+
+
+app.post("/conversation", async(req, res) => {
+
+    // get the query from the user
+
+
+})
+
+
+app.listen(3000, () => {
+    console.log('Server is running on port 3000');
+});
