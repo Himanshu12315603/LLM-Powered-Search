fb7ec2904db7551fa9a0aa093b046e249ce61201
Himanshu12315603 <himanshukumar7349820018@gmail.com>
2026-05-13 01:29:39 +0530

    add: basic of checks

--- backend/middleware.ts
+++ backend/middleware.ts
@@ -1,5 +1,19 @@
 import type { Request, Response, NextFunction } from 'express';
+import { createSupabaseClient } from './client';
 
-export function middleware(req: Request, res: Response, next: NextFunction) {
 
+const client = createSupabaseClient(); 
+export async function middleware(req: Request, res: Response, next: NextFunction) {
+    const token = req.headers.authorization;
+
+    const data = await client.auth.getUser(token);
+
+    const userId = data.data.user?.id;
+
+    if(userId) {
+        req.userId = userId;
+        next();
+    }else {
+        res.status(401).json({error: "Unauthorized"});
+    }
 }
