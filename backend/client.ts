1a5b01c7fda49adeca5657209e09d20c389138cd
Himanshu12315603 <himanshukumar7349820018@gmail.com>
2026-05-22 12:37:11 +0530

    fix auth and env problem

--- backend/client.ts
+++ backend/client.ts
@@ -2,7 +2,7 @@ import { createClient } from '@supabase/supabase-js'
 
 export function createSupabaseClient() {
   return createClient(
-    process.env.BUN_PUBLIC_SUPABASE_URL!,
-    process.env.BUN_PUBLIC_SUPABASE_PUBLISHABLE_KEY!
+    import.meta.env.BUN_PUBLIC_SUPABASE_URL!,
+    import.meta.env.BUN_PUBLIC_SUPABASE_PUBLISHABLE_KEY!
   )
 }
