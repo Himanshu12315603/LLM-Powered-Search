78533e613234900ee860992ae7891a088e8b10c6
Himanshu12315603 <himanshukumar7349820018@gmail.com>
2026-05-22 12:45:46 +0530

    fix client env problem

--- frontend/src/lib/supabase/client.ts
+++ frontend/src/lib/supabase/client.ts
@@ -2,7 +2,7 @@ import { createBrowserClient } from '@supabase/ssr'
 
 export function createClient() {
   return createBrowserClient(
-    process.env.BUN_PUBLIC_SUPABASE_URL!,
-    process.env.BUN_PUBLIC_SUPABASE_PUBLISHABLE_KEY!
+    import.meta.env.BUN_PUBLIC_SUPABASE_URL!,
+    import.meta.env.BUN_PUBLIC_SUPABASE_PUBLISHABLE_KEY!
   )
 }
